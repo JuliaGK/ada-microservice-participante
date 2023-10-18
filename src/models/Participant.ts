b158d5a4b9090ce3578095999d40c830d5d22b1f
Júlia Küchle <juliagkuchle@gmail.com>
2023-10-18 15:46:09 -0300

    add participant model

--- src/models/Participant.ts
+++ src/models/Participant.ts
@@ -2,4 +2,9 @@ class Participant {
     id?: string;
     idEvento: string;
     idUser: string;
+
+    constructor(idEvento: string, idUser: string) {
+        this.idEvento = idEvento;
+        this.idUser = idUser;
+    }
 }
